387b66e2b090d5f90dcbc1dc7630981b0cc4fa70
Steve Dias <stevediaspro@gmail.com>
2022-05-10 12:20:01 +0200

    menu annimation close and open set

--- components/Header.js
+++ components/Header.js
@@ -1,89 +1,131 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Logo from "./../public/logo.svg";
 import LogoBlanc from "./../public/logo-blanc.svg";
-import { useTheme } from '../lib/ThemeContext';
-import {isMobile} from 'react-device-detect';
+import { useTheme } from "../lib/ThemeContext";
+import { isMobile } from "react-device-detect";
 
 const NavItem = (props) => {
   const [open, setOpen] = useState(false);
   const { themeBlack, setThemeblack } = useTheme();
-  useEffect(() =>{
-    if (open){
+  const [animationClose, setAnimationClose] = useState(false);
+
+  const close = async (ms) => {
+    setAnimationClose(true);
+
+    await new Promise((r) => setTimeout(r, ms));
+
+    
+    setAnimationClose(false);
+    setOpen(false);
+  };
+
+  const handleOpenCloseMenu = () => {
+    if (open) {
+      close(300);
+    } else {
+      setOpen(true);
+      setAnimationClose(false)
+    }
+  };
+  useEffect(() => {
+    if (open) {
       document.body.style.overflow = "hidden";
-    }else{
+    } else {
       document.body.style.overflow = "auto";
     }
-    
-  },[open])
-  if(isMobile){
+  }, [open]);
+  if (isMobile) {
     return (
-      <div className={`nav-item ${ open ? 'nav-item-background-white' : '' }`}>
-      <div
-        className="item-list"
-      >
-        <button className={`menu-button ${ themeBlack ? 'sub-menu-black-button':'sub-menu-white-button' }` } onClick={() => setOpen(!open)}>
-          {open ? 'FERMER' : props.name}
-        </button>
-        {open && props.children}
+      <div className={`nav-item ${open ? "nav-item-background-white open-animation " : ''} ${animationClose? 'menu-close-animation': ''} `}>
+        <div className="item-list">
+          <button
+            className={`menu-button ${
+              themeBlack ? "sub-menu-black-button" : "sub-menu-white-button"
+            }`}
+            onClick={() => handleOpenCloseMenu()}
+          >
+            {open ? "FERMER" : props.name}
+          </button>
+          <div className={`sub-menu  `}>
+            <ul>{open && props.children}</ul>
+          </div>
+        </div>
       </div>
-    </div>
     );
-  }else{
-  return (
-    <div className="nav-item">
-      <div
-        className="item-list"
-        onMouseEnter={() => setOpen(true)}
-        onMouseLeave={() => setOpen(false)}
-      >
-        <button className={`menu-button ${ themeBlack ? 'sub-menu-black-button':'sub-menu-white-button' }` } onClick={() => setOpen(!open)}>
-          {open ? 'Fermer': props.name}
-        </button>
-        {open && props.children}
+  } else {
+    return (
+      <div className={`nav-item ${open ? 'open-animation':''} ${animationClose ? 'menu-close-animation': ''}` }>
+        <div
+          className="item-list"
+          onMouseEnter={() => handleOpenCloseMenu()}
+          onMouseLeave={() => handleOpenCloseMenu()}
+        >
+          <button
+            className={`menu-button ${
+              themeBlack ? "sub-menu-black-button" : "sub-menu-white-button"
+            }`}
+            onClick={() => handleOpenCloseMenu()}
+          >
+            {open ? "Fermer" : props.name}
+          </button>
+          <div className={`sub-menu `}>
+            <ul>{open && props.children}</ul>
+          </div>
+        </div>
       </div>
-    </div>
-  );}
+    );
+  }
 };
 
 export default function Header() {
   const { themeBlack, setThemeblack } = useTheme();
   return (
-    <nav className={`menu-container global-container nav-global-container ${ themeBlack ? 'menu-black-color' : 'menu-white-color'}`}>
+    <nav
+      className={`menu-container global-container nav-global-container ${
+        themeBlack ? "menu-black-color" : "menu-white-color"
+      }`}
+    >
       <div className="content-container menu-principal">
         <div className="menu-columns menu-left">
           <NavItem name={"Menu"}>
-            <div className="sub-menu">
-
-              <ul>
-                <li>
-
-                  <div className={`sub-menu-item `}>
-                    <span className={`${themeBlack? '' : 'sub-menu-item-white'}`}>Collections</span>
-                  </div>
-                </li>
-                <li>
-                  <div className="sub-menu-item">
-                    <span className={`${themeBlack? '' : 'sub-menu-item-white'}`}>Shootbook</span>
-                  </div>
-                </li>
-                <li>
-                  <div className="sub-menu-item">
-                    <span className={`${themeBlack? '' : 'sub-menu-item-white'}`}>A propos de UNADN</span>
-                  </div>
-                </li>
-                <li>
-                  <div className="sub-menu-item">
-                    <span className={`${themeBlack? '' : 'sub-menu-item-white'}`}>Nous contacter</span>
-                  </div>
-                </li>
-              </ul>
-            </div>
+            <li>
+              <div className={`sub-menu-item `}>
+                <span className={`${themeBlack ? "" : "sub-menu-item-white"}`}>
+                  Collections
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="sub-menu-item">
+                <span className={`${themeBlack ? "" : "sub-menu-item-white"}`}>
+                  Shootbook
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="sub-menu-item">
+                <span className={`${themeBlack ? "" : "sub-menu-item-white"}`}>
+                  A propos de UNADN
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="sub-menu-item">
+                <span className={`${themeBlack ? "" : "sub-menu-item-white"}`}>
+                  Nous contacter
+                </span>
+              </div>
+            </li>
           </NavItem>
         </div>
         <div className="menu-columns menu-center">
           <div className="img-wrapper">
-            <Image className={"logo"} src={themeBlack ? Logo: LogoBlanc} layout="fill" />
+            <Image
+              className={"logo"}
+              src={themeBlack ? Logo : LogoBlanc}
+              layout="fill"
+            />
           </div>
         </div>
         <div className="menu-columns menu-right">Panier(0)</div>
