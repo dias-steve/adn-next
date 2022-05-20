9081a3af0cec5acff5f4af873f464b73aa337066
Steve Dias <stevediaspro@gmail.com>
2022-05-20 14:26:29 +0200

    useRef

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -1,4 +1,4 @@
-import React, { useRef }  from 'react';
+import React, { useRef }  from "react";
 
 import ProductForm from '../../components/ProductForm';
 import ProductImageList from '../../components/ProductImageList';
