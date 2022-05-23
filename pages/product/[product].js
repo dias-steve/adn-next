b1f87c39beb22200505c78c0f3794e4f0e8e1dd6
Steve Dias <stevediaspro@gmail.com>
2022-05-23 21:23:40 +0200

    revalidate fixing

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -204,6 +204,7 @@ export async function getStaticProps(context) {
       product,
       key: uuidv4()
     },
+    revalidate: 60
   };
 }
 
@@ -215,7 +216,7 @@ export async function getStaticPaths() {
   // on dit le chemin pour chaque articles
   const paths = products.map((item) => ({
     params: { product: item.id.toString() },
-    revalidate: 60,
+   
   }));
 
   return {
