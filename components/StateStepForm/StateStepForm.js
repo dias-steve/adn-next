892ff9f8029ef6e8a7bc4e7537ee39be7a5d9f52
Steve Dias <stevediaspro@gmail.com>
2022-07-06 21:55:30 +0200

    checkout process done

--- components/StateStepForm/StateStepForm.js
+++ components/StateStepForm/StateStepForm.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./StateStepForm-styles.module.scss";
+
+
 export default function StateStepForm({ currentStep = 1 }) {
   const width = currentStep * 33.5 + "%";
+  const [up, setUp] = useState(false);
+  const [y, setY] = useState(0);
+
+  const handleNavigation = (e) => {
+    const window = e.currentTarget;
+    if (y-300 > window.scrollY || y <=0) {
+  
+        setUp(false);
+        setY(window.scrollY);
+    } else if (y+100 < window.scrollY) {
+
+    setUp(true);
+    setY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    setY(window.scrollY);
+   
+    window.addEventListener("scroll", (e) => handleNavigation(e));
+    return window.removeEventListener("scroll",  (e) => handleNavigation(e))
+  }, [y]);
+
   return (
-        <div className={styles.containerGlobal}>
+        <div className={[styles.containerGlobal, (up ? styles.headerSmall :  styles.headerBig)].join(" ")}>
         <div className={[styles.stepWrapper].join("")}>
             <div className={styles.stepTextWrapper}>
             <span
