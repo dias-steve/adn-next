a1718c04ef3bde92c9c181ae57f6c5bb8e6f6e23
Steve Dias <stevediaspro@gmail.com>
2022-07-08 21:18:09 +0200

    change

--- components/videoViewer/VideoViewer.js
+++ components/videoViewer/VideoViewer.js
@@ -3,19 +3,16 @@ import React, { useEffect, useRef } from 'react';
 import styles from "./VideoViewer-component-styles.module.scss";
 
 export default function VideoViewer({ video }) {
-    const videoRef = useRef();
 
-    useEffect(() => {
-        setTimeout(()=>{
-            videoRef.current.play()
-        },5000)
-    }, []);
   return (
     <div className={styles.containerGlobal}>
       <div className={styles.videoBlock}>
         <div className={styles.videoContainer}>
           <div className={styles.videoWrapper}>
-            <video ref={videoRef} src={video.url}  className={styles.videoViwer}/>
+            <video  src={video.url}         playsinline=""
+        autoPlay
+        webkit-playsinline=""
+        loop  className={styles.videoViwer}/>
  
 
           </div>
