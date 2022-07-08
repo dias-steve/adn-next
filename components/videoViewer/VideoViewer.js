2504987d9cb6110e2edadd28b8ccbbd5a89c2101
Steve Dias <stevediaspro@gmail.com>
2022-07-08 21:58:16 +0200

    video google test

--- components/videoViewer/VideoViewer.js
+++ components/videoViewer/VideoViewer.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useRef } from 'react';
 import styles from "./VideoViewer-component-styles.module.scss";
 
+const videogoogle = "https://kstatic.googleusercontent.com/files/c44f15bb7e678d651e18fdee3058f2948aa733849e0dea3daf7429bf0f77ec23bd670dba63e71739d5b53489c98689bdbb80c47cf55f44649d9d1bfdf3e4f0a0";
 export default function VideoViewer({ video }) {
 
   return (
@@ -9,7 +10,7 @@ export default function VideoViewer({ video }) {
       <div className={styles.videoBlock}>
         <div className={styles.videoContainer}>
           <div className={styles.videoWrapper}>
-            <video  src={video.url}         playsinline=""
+            <video  src={videogoogle }         playsinline=""
         autoPlay
         webkit-playsinline=""
         loop  className={styles.videoViwer}/>
