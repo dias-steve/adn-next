58303bc82572ed0791b8308db755901f093e2221
Steve Dias <stevediaspro@gmail.com>
2022-07-09 14:23:05 +0200

    fix: video

--- components/videoViewer/VideoViewer.js
+++ components/videoViewer/VideoViewer.js
@@ -1,17 +1,40 @@
 
 import { is } from '@react-spring/shared';
 import React, { useEffect, useRef, useState } from 'react';
+import Image from "next/image";
 import styles from "./VideoViewer-component-styles.module.scss";
+import iconPlay from "../../public/play-fill.svg"
 const PLAYING_DEBOUNCE_TIME = 50;
 const WAITING_DEBOUNCE_TIME = 200;
 const videogoogle = "https://kstatic.googleusercontent.com/files/c44f15bb7e678d651e18fdee3058f2948aa733849e0dea3daf7429bf0f77ec23bd670dba63e71739d5b53489c98689bdbb80c47cf55f44649d9d1bfdf3e4f0a0";
 
+export const BtnPlay = ({onClick}) => {
+  return (
+    <div onClick={() => onClick()} className={styles.btnPlayContainer}>
+
+      <div className={styles.imageBtnWrapper}>
+                <Image src={ iconPlay } alt= { 'icon Play video' } layout="fill" className={styles.image} />
+            </div>
+    </div>
+  )
+}
+
+export const SpinWaiting = () => {
+  return(
+  <div className={styles.btnPlayContainer}>
+        <div className={styles.wrapperSpinner}>
+          <div className={styles.spin}></div>
+      </div>
+  </div>
+  )
+}
 export default function VideoViewer({ video }) {
-  const [isLoading, setIsLoading] = useState(true)
+
 
  
     const [isPlaying, setIsPlaying] = useState(false);
     const [isWaiting, setIsWaiting] = useState(false);
+    const [loaded, setLoaded] = useState(false);
   
     const isWaitingTimeout = useRef(null);
     const isPlayingTimeout = useRef(null);
@@ -51,12 +74,16 @@ export default function VideoViewer({ video }) {
         }, PLAYING_DEBOUNCE_TIME);
       };
   
+      const loadeddataHandle = () => {
+        setLoaded(true)
+      }
       const element = videoElementRef.current;
   
       element.addEventListener("waiting", waitingHandler);
       element.addEventListener("play", playHandler);
       element.addEventListener("playing", playHandler);
       element.addEventListener("pause", pauseHandler);
+      element.addEventListener("loadeddata", loadeddataHandle);
   
       // clean up
       return () => {
@@ -70,6 +97,7 @@ export default function VideoViewer({ video }) {
       };
     }, [videoElementRef]);
   
+    
     const handlePlayPauseClick = () => {
       if (videoElementRef.current) {
         if (isPlaying) {
@@ -83,15 +111,23 @@ export default function VideoViewer({ video }) {
  
   return (
     <div className={styles.containerGlobal}>
+      {!isPlaying && loaded && <BtnPlay onClick = {() => {handlePlayPauseClick()}}/>}
+      { isWaiting &&
+        <SpinWaiting/>
+      }
 
-    
+      {!loaded &&
+        <SpinWaiting/>
+      }
       <div className={styles.videoBlock}>
+     
         <div className={styles.videoContainer}>
           <div className={styles.videoWrapper}>
-            <video ref={videoElementRef} src={video.url}         playsinline=""
+            <video  onClick = {() => {handlePlayPauseClick()}}ref={videoElementRef} src={video.url}         playsinline=""
         autoPlay
         preload={'auto'}
         webkit-playsinline=""
+        muted
         loop  className={styles.videoViwer}
         
         />
