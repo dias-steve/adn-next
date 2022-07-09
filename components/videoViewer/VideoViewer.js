8da83384df91eea792e5f4436a0d372dd58103ef
Steve Dias <stevediaspro@gmail.com>
2022-07-09 18:33:20 +0200

    reformat product

--- components/videoViewer/VideoViewer.js
+++ components/videoViewer/VideoViewer.js
@@ -3,7 +3,7 @@ import { is } from '@react-spring/shared';
 import React, { useEffect, useRef, useState } from 'react';
 import Image from "next/image";
 import styles from "./VideoViewer-component-styles.module.scss";
-import iconPlay from "../../public/play-fill.svg"
+import iconPlay from "../../public/play-fill-green.svg"
 const PLAYING_DEBOUNCE_TIME = 50;
 const WAITING_DEBOUNCE_TIME = 200;
 const videogoogle = "https://kstatic.googleusercontent.com/files/c44f15bb7e678d651e18fdee3058f2948aa733849e0dea3daf7429bf0f77ec23bd670dba63e71739d5b53489c98689bdbb80c47cf55f44649d9d1bfdf3e4f0a0";
