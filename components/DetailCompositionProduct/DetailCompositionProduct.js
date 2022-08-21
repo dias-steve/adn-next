277471dd677628659ed882362a7e146b89f5792f
Steve Dias <stevediaspro@gmail.com>
2022-08-21 11:47:39 +0200

    reformat scss composition

--- components/DetailCompositionProduct/DetailCompositionProduct.js
+++ components/DetailCompositionProduct/DetailCompositionProduct.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Image from "next/image";
 import { v4 as uuidv4 } from "uuid";
+import styles from './DetailCompositionProduct.module.scss'
 const CompositionItem = ({img, desc}) => {
 
-  return(<div className='composition-item-container'>
-  <div className='composition-item-content-left'>
-  <div  className="image-wrapper">
-    <Image src={img.url} alt={img.alt} layout={"fill"} className={"image"}/>
+  return(<div className={styles.compositionItemContainer}>
+  <div className={styles.compositionItemContentLeft}>
+  <div  className={styles.imageWrapper}>
+    <Image src={img.url} alt={img.alt} layout={"fill"} className={styles.image}/>
   </div>
   </div>
-  <div className='composition-item-content-right'>
-    <p className='composition-item-description'>
+  <div className={styles.compositionItemContentRight}>
+    <p>
       {desc}
     </p>
   </div>
@@ -22,10 +23,10 @@ export default function DetailCompositionProduct({data}) {
     <>
     {
       build_list && Array.isArray(build_list) &&
-    <div className="detailCompositionProduct-conatainer">
+    <div className={styles.detailCompositionProductContainer}>
 
-      <h2 className="detailCompositionProduct-title"> Composition </h2>
-      <div className='composition-list-item-wrapper'>
+      <h2 className={styles.detailCompositionProductTitle}> Composition </h2>
+      <div className={styles.compositionListItemWrapper}>
       
      
             {build_list.map(build => (
