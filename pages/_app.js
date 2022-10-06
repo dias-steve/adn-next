8b34dfa3d697886b77f43004b117103c97bac8e7
Steve Dias <stevediaspro@gmail.com>
2022-10-06 16:26:39 +0200

    [DELETE] Remove google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -36,26 +36,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-         {/* Global Site Tag (gtag.js) - Google Analytics       */}
-         
-         <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+
 
 
 
